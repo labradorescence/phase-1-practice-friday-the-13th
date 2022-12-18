caea34e7d7f393cb4a1684c5980ca78ece6f5315
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-12-18 12:00:32 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
+let movieArr;
+let currentMovie;
+
 fetch("http://localhost:3000/movies")
     .then(resp => resp.json())
     .then(movies => {
-        movieList(movies)
-        movieDetail(movies)
+        movieArr = movies
+
+        movieImgListNav(movieArr)
+        movieDetail(movieArr[0])
+        watchedBtnFunc()
     })
 
 //Challenge 1
 const movieListNav = document.querySelector("#movie-list")
     
-const movieList = (movies) => {
-    movies.map(eachMovie => {
+const movieImgListNav = (movieArr) => {
+    movieArr.map(eachMovie => {
         //console.log(eachMovie)
         const movieImg = document.createElement("img")
         movieImg.src = eachMovie.image
         movieImg.alt = eachMovie.title
         movieImg.id = eachMovie.title
         movieListNav.append(movieImg)
+
+
+        //challenge 3
+        movieImg.addEventListener("click", () => {
+            movieDetail(eachMovie) //we have current movie info so we use that "here" 
+        })
     })
 }
 
@@ -24,20 +36,38 @@ const detailImage = document.querySelector("#detail-image")
 const title = document.querySelector("#title")
 const year = document.querySelector("#year-released")
 const description = document.querySelector("#description")
+const watched = document.querySelector("#watched")
+const blood = document.querySelector("#amount")
+const navImg = document.querySelector("#movie-list")
 
-const movieDetail = (movies) => {
-    movies.map(eachMovie => {
-        detailImage.src = eachMovie.image
-        title.innerText = eachMovie.title
-        year.innerHTML = eachMovie.release_year
-        description.innerText = eachMovie.description
+const movieDetail = (movie) => {  
+    currentMovie = movie
+
+    detailImage.src = movie.image
+    title.innerText = movie.title
+    year.innerHTML = movie.release_year
+    description.innerText = movie.description
+    movie.watched === false? watched.innerText = "Unwatched":watched.innerText = "Watched"
+    blood.innerText = movie.blood_amount
+}
+
+//challenge 4 
+const watchedBtnFunc = () => {
+    watched.addEventListener("click", () => {
+        currentMovie.watched = !currentMovie.watched
+        watched.innerHTML = currentMovie.watched? "Watched":"Unwatched"
     })
 }
 
-//challenge 3
-const navImg = document.getElementById("movie-list")
 
-navImg.addEventListener("click", (e) => {
-    console.log(e.target.alt)
-    
+//challenge 5
+const form = document.querySelector("#blood-form")
+const input = document.querySelector("#blood-amount")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    currentMovie.blood_amount += parseInt(input.value)
+    console.log(currentMovie.blood_amount)
+    blood.textContent = currentMovie.blood_amount
+    e.target.reset();
 })
