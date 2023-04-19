5ef1e2de4da9e299a08dbc5009aa3d20f3e3c36c
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-04-18 21:18:05 -0500

    greems solution

--- src/index.js
+++ src/index.js
@@ -1,194 +1,138 @@
+// ## Challenge 1
+// For each movie returned from `http://localhost:3000/movies` create an image and add it to the `movie-list` nav element.
 addEventListener('DOMContentLoaded', () => {
-  /**********DELIVERABLES START*****************/
-  //   Challenge 1
-  //   For each movie returned from http://localhost:3000/movies create an image and add it to the movie-list nav element.
+  let movieArr;
+  let currentMovie;
 
-  //   Challenge 2
-  //   As soon as the page loads, we should see the details of the first movie in the dataset.
+  //FETCH
 
-  //   Challenge 3
-  //   When you click on each movie image in the top nav, you should populate the detail area with the image, title, release_year, description, watched, and blood_amount for the movie that was clicked.
+  //GET
+  function getMovies() {
+    return fetch('http://localhost:3000/movies')
+      .then((resp) => resp.json())
+      .then((movies) => {
+        movieArr = movies;
+        currentMovie = movieArr[0];
 
-  //   If the value of 'watched' is false, the button should say 'Unwatched'. If the value is true, then the button should say 'Watched'.
+        movieArr.map((movie) => {
+          addImgToNav(movie);
+        });
 
-  //   Challenge 4
-  //   When you click on the button in the details it should toggle between Watched or Unwatched depending on the value of watched for the movie currently being displayed.
+        movieDetail(currentMovie); //invoking the movie detail
+      });
+  }
 
-  //   The watched value should stay the same when you click between the different movies.
+  //0. select the nav element
+  const movieList = document.querySelector('nav#movie-list');
 
-  //   Challenge 5
-  //   On the right side there's a form that allows the user to enter a number of blood drops to add to each movie (don't ask why). For each movie, I should be able to add more drops.
+  function addImgToNav(movie) {
+    //1. create an img element
+    const movieImg = document.createElement('img');
 
-  //   Example:
+    //2. assign the movie img src
+    // console.log(movie)
+    movieImg.src = movie.image;
+    movieImg.alt = movie.title;
 
-  //   If the value is 0 and I enter 10, then number of drops for the movie should be 10.
-  //   If the value is 20 and I enter 5, then the number of drops for the movie should be 25.
-  //   The blood amount value should stay the same when you click between the different movies.
-  /**********DELIVERABLES END*******************/
+    //3. append the img to the nav element
+    movieList.append(movieImg);
 
-  /**********VARIABLE DECLARATION START*********/
-  const baseUrl = 'http://localhost:3000/movies/';
-  const movieList = document.querySelector('#movie-list');
-  let movieArray = [];
-  const movieDetail = document.querySelector('#movie-info');
+    movieImg.addEventListener('click', () => {
+      movieDetail(movie);
+      currentMovie = movie;
+    });
+  }
+
+  // ## Challenge 2
+  // As soon as the page loads, we should see the details of the **first** movie in the dataset.
+  //1. select the elements
+  const imgDetail = document.querySelector('#movie-info #detail-image');
+  const title = document.querySelector('#title');
+  const year = document.querySelector('#movie-info #year-released');
+  const description = document.querySelector('#movie-info #description');
+  const watched = document.querySelector('#movie-info #watched');
+  const blood = document.querySelector('#movie-info #amount');
+
+  const movieDetail = (m) => {
+    // console.log(m)
+    //2. add the value using the data
+    imgDetail.src = m.image;
+    title.textContent = m.title;
+    year.textContent = m.release_year;
+    description.textContent = m.description;
+    watched.textContent = m.watched ? 'Watched' : 'Unwatched'; //truthy:falsy
+    blood.textContent = m.blood_amount;
+
+    watched.addEventListener('click', () => {
+      m.watched = !m.watched;
+      watched.textContent = m.watched ? 'Watched' : 'Unwatched';
+    });
+  };
+  // ## Challenge 3
+  // When you click on each movie image in the top nav, you should populate the detail area with the `image`, `title`, `release_year`, `description`, `watched`, and `blood_amount` for the movie that was clicked.
+
+  // If the value of 'watched' is false, the button should say 'Unwatched'. If the value is true, then the button should say 'Watched'.
+
+  // ## Challenge 4
+  // When you click on the button in the details it should toggle between `Watched` or `Unwatched` depending on the value of `watched` for the movie currently being displayed.
+
+  // _The watched value should stay the same when you click between the different movies._
+
+  // ## Challenge 5
+  // On the right side there's a form that allows the user to enter a number of blood drops to add to each movie (don't ask why). For each movie, I should be able to add more drops.
+
+  // Example:
+  // - If the value is 0 and I enter 10, then number of drops for the movie should be 10.
+  // - If the value is 20 and I enter 5, then the number of drops for the movie should be 25.
+
+  // _The blood amount value should stay the same when you click between the different movies._
+
+  //1. select the element
+  //2. grab the blood amount from the form
+  //3. add the blood amount
+
   const bloodForm = document.querySelector('#blood-form');
-  const bloodInput = document.querySelector('#blood-amount');
-  /**********VARIABLE DECLARATION END***********/
+  const bloodAmount = document.querySelector('#blood-amount');
 
-  /**********FETCH REQUESTS START***************/
-  const rover = {
-    fetch: function getJSON(url) {
-      return fetch(url)
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw response.statusText;
-          }
-        })
-        .catch((error) => console.log(error.message));
-    },
+  function getBloodAmount() {
+    bloodAmount.addEventListener('change', (e) => {
+      submitForm(e.target.value);
+    });
+  }
 
-    post: function postJSON(url, data) {
-      return fetch(url, {
-        method: 'POST',
+  // function submitForm(bloodAmount){
+  //     bloodForm.addEventListener("submit", (e) => {
+  //         e.preventDefault()
+  //         console.log(bloodAmount)
+  //         currentMovie.blood_amount += Number(bloodAmount)
+  //         blood.textContent = currentMovie.blood_amount
+  //         e.target["blood-amount"].value = ""
+  //     })
+  // }
+
+  function submitForm(bloodAmount) {
+    bloodForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      currentMovie.blood_amount += parseInt(bloodAmount);
+
+      const data = { blood_amount: currentMovie.blood_amount };
+
+      fetch(`http://localhost:3000/movies/${currentMovie.id}`, {
+        method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
       })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw response.statusText;
-          }
-        })
-        .catch((error) => console.log(error.message));
-    },
-    //DELETE URL
-    delete: function deleteJSON(url) {
-      return fetch(url, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw response.statusText;
-          }
-        })
-        .catch((error) => console.log(error.message));
-    },
-  };
-  /**********FETCH REQUESTS END*****************/
+        .then((resp) => resp.json())
+        .then((data) => console.log(data))
+        .catch(console.error);
 
-  /**********EVENT LISTENERS START**************/
-  bloodInput.addEventListener('change', (e) => console.log(e.target.value));
-  bloodForm.addEventListener('submit', (e) => {
-    event.preventDefault();
-    let newBloodAmount =
-      parseInt(document.querySelector('#movie-info #amount').textContent) +
-      parseInt(e.target['blood-amount'].value);
-
-    document.querySelector('#movie-info #amount').textContent = newBloodAmount;
-  });
-  /**********EVENT LISTENERS END****************/
-
-  /**********FORM PROCESSING START**************/
-  //handle input from search form
-  function handleForm(event) {
-    event.preventDefault();
-    console.log(event);
-  }
-
-  function handleClick(event) {
-    console.log(event);
-  }
-  /**********FORM PROCESSING END****************/
-
-  /**********DOM RENDER FUNCTIONS START*********/
-  function renderImages(movie) {
-    //create elements
-    let movieImg = document.createElement('img');
-    //populate data, text, classes, attributes
-    movieImg.src = movie.image;
-    movieImg.alt = movie.title;
-    //append elements to the DOM
-    movieList.append(movieImg);
-
-    movieImg.addEventListener('click', () => {
-      renderMovie(movie);
+      blood.textContent = currentMovie.blood_amount;
+      e.target['blood-amount'].value = '';
     });
   }
 
-  const renderMovie = (movie) => {
-    // <div id="movie-info">
-    //   <img id="detail-image" src="" />
-    //   <div>
-    //     <h1 id="title">Title</h1>
-    //     <h3 id="year-released">Year Released</h3>
-    //     <p id="description">Description</p>
-    //     <button id="watched">Watched</button>
-    //     <p>
-    //       <img class="icon" src="./assets/blood-drop.png" />x
-    //       <span id="amount">0</span>
-    //create elements
-    const movieImage = document.querySelector('#movie-info #detail-image');
-    const movieTitle = document.querySelector('#movie-info #title');
-    const movieYear = document.querySelector('#movie-info #year-released');
-    const movieDescription = document.querySelector('#movie-info #description');
-    const movieWatched = document.querySelector('#movie-info #watched');
-    const bloodAmount = document.querySelector('#movie-info #amount');
-    //populate data, text, classes, attributes
-    movieImage.src = movie.image;
-    movieTitle.textContent = movie.title;
-    movieYear.textContent = movie.released_year;
-    movieDescription.textContent = movie.description;
-    movieWatched.textContent = movie.watched ? 'Watched' : 'Not Watched';
-    bloodAmount.textContent = movie.blood_amount;
-    //append elements to the DOM
-    watched.addEventListener('click', () => {
-      movie.watched = !movie.watched;
-      movieWatched.textContent = movie.watched ? 'Watched' : 'Not Watched';
-      console.log('clicked');
-    });
-  };
-
-  /**********DOM RENDER FUNCTIONS END***********/
-
-  /***********GENERAL FUNCTIONS START***********/
-
-  function getMovies() {
-    rover.fetch(baseUrl).then((movies) => {
-      movieArray = movies;
-
-      movieArray.map((movie) => {
-        currentMovie = movie;
-        renderImages(currentMovie);
-      });
-
-      renderMovie(currentMovie);
-    });
-  }
-
-  //greem's code
-  //   function getMovies() {
-  //     return fetch(baseUrl)
-  //       .then((resp) => resp.json())
-  //       .then((movies) => {
-  //         movieArray = movies;
-  //         movieArray.map((movie) => {
-  //           renderImages(movie);
-  //         });
-  //       });
-  //   }
   getMovies();
-  function deleteMovie(id) {
-    rover.delete(`${baseUrl}/${id}`).then((movie) => console.log(movie));
-  }
-  /***********GENERAL FUNCTIONS END*************/
+  getBloodAmount();
 });
